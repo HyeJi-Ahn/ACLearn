55e64a22d651440091e871ed6ee1e9957bae1f02
[itsnew30] <[itsnew30@gmail.com]>
2022-11-28 18:11:31 +0900

    User[login, join, 아이디 찾기]
    - login 기능
    - join 기능
    - 아이디 찾기 기능

--- src/main/java/com/ezen/aclearn/service/user/UserService.java
+++ src/main/java/com/ezen/aclearn/service/user/UserService.java
@@ -1,5 +1,26 @@
 package com.ezen.aclearn.service.user;
 
-public interface UserService {
+import com.ezen.aclearn.vo.UserVO;
 
+public interface UserService {
+	// 회원가입
+	int join(UserVO userVO);
+	
+	// 로그인
+	UserVO login(UserVO userVO);
+	
+	// 아이디 중복체크
+	int idCheck(String userId);
+	
+	// 아이디 찾기
+	String searchId(UserVO searchVO);
+		
+	// 비밀번호 찾기
+	UserVO searchPw(UserVO searchVO);
+	
+	// 닉네임 중복체크
+	int nicknameCheck(String userNickname);
+	
+	
+	
 }
